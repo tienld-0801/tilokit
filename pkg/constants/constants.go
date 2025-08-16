632985b2209e92d351cec01fd15815025e2fa4a3
tienld <taylortien99@gmail.com>
2025-08-16 19:07:19 +0700

    🐛 fix: down version v7

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -4,7 +4,7 @@ import "runtime"
 
 // Version information - set during build time
 var (
-	Version   = "v0.1.8-dev"
+	Version   = "v0.1.7-dev"
 	BuildDate = "unknown"
 	GitCommit = "unknown"
 	GoVersion = runtime.Version()
