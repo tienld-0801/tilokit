bfe40d9c24cb399900845981066dbf245c769106
tienld <taylortien99@gmail.com>
2025-08-16 19:28:12 +0700

    🚀 release: v0.2.1-dev
    - Update version to v0.2.1-dev in constants.go
    - Update README.md version badge and contributors
    - Ready for release process

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -4,7 +4,7 @@ import "runtime"
 
 // Version information - set during build time
 var (
-	Version   = "v0.2.0-dev"
+	Version   = "v0.2.1-dev"
 	BuildDate = "unknown"
 	GitCommit = "unknown"
 	GoVersion = runtime.Version()
